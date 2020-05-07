1549c408dbaca88c7765f03d75d8bfbe006cff0c
Claudio Bantaloukas <rockdreamer@gmail.com>
2020-05-07 11:29:17 +0100

    import conanfile from conan-center-index, add version

--- conanfile.py
+++ conanfile.py
@@ -2,61 +2,67 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    description = "Keep it short"
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports_sources = ["CMakeLists.txt"]
+class LuaConan(ConanFile):
+    name = "lua"
+    description = "Lua is a powerful, efficient, lightweight, embeddable scripting language."
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://www.lua.org/"
+    topics = ("conan", "lua", "scripting")
+    license = "MIT"
+    version = "5.2.4"
     generators = "cmake"
+    settings = "os", "compiler", "arch", "build_type"
+    exports_sources = ["CMakeLists.txt", "patches/**"]
+    options = {"shared": [False, True], "fPIC": [True, False], "compile_as_cpp": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "compile_as_cpp": False}
 
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
-    _cmake = None
-
-    requires = (
-        "zlib/1.2.11"
-    )
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
+        extracted_dir = "lua-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if not self.options.compile_as_cpp:
+            del self.settings.compiler.libcxx
+            del self.settings.compiler.cppstd
 
     def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)
-            self._cmake.definitions["BUILD_TESTS"] = False  # example
-            self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+        cmake = CMake(self)
+        cmake.definitions["SOURCE_SUBDIR"] = self._source_subfolder
+        cmake.definitions["SKIP_INSTALL_TOOLS"] = True
+        cmake.definitions["COMPILE_AS_CPP"] = self.options.compile_as_cpp
+        cmake.configure()
+        return cmake
 
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        # Extract the License/s from the header to a file
+        tmp = tools.load( os.path.join(self._source_subfolder, "src", "lua.h") )
+        license_contents = tmp[tmp.find("/***", 1):tmp.find("****/", 1)]
+        tools.save(os.path.join(self.package_folder, "licenses", "COPYING.txt"), license_contents)
         cmake = self._configure_cmake()
         cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs.append(os.path.join("include", "lua"))
+        if self.settings.os in ["Linux", "Macos"]:
+            self.cpp_info.defines.extend(["LUA_USE_DLOPEN", "LUA_USE_POSIX"])
+            self.cpp_info.system_libs = ["m"]
+            if self.settings.os != "Macos":
+                self.cpp_info.system_libs.append("dl")
+        elif self.settings.os == "Windows" and self.options.shared:
+            self.cpp_info.defines.append("LUA_BUILD_AS_DLL")
